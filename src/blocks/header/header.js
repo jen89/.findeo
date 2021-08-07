85d2f243cdac6361fac5162f13b4a906c4902a4b
jen89 <jenchik_1989@bk.ru>
2021-08-07 23:25:40 +0700

    one

---
+++ src/blocks/header/header.js
@@ -0,0 +1,29 @@
+// import Swiper from 'swiper/bundle';
+import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+SwiperCore.use([Navigation, Pagination]);
+import "fancybox";
+
+(() => {
+    $('.header__menu-opened, .header__menu-close').on('click', function (e) {
+        e.preventDefault();
+        $('.header__menu').toggleClass('active');
+    });
+
+    //new Swiper('.swiper-container');
+    new SwiperCore('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+            delay: 2000,
+        },
+        effect: 'fade',
+        loop: true,
+
+    });
+
+    // $('.header-slider__btnwatch').fancybox({});
+    $('[data-fancybox]').fancybox({});
+
+})();
